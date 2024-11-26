3fa990be3807ec6ecd2438a3ff6f31753328d99b
bfaatime <fatimebexseliyeva@icloud.com>
2024-11-26 18:51:08 +0400

    added

---
+++ 26.11.2024/assets/js/main.js
@@ -0,0 +1,31 @@
+import { BASE_URL } from "./constants.js";
+
+function getData(endpoint) {
+    fetch(`${BASE_URL}/${endpoint}`)
+        .then((responsive) => responsive.json())
+        .then((data) => {
+        cards(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally ( () => {});
+}
+
+function cards(arr){
+    const rowContainer = document.querySelector(".row");
+    rowContainer.innerHTML =""
+    arr.forEach((item) => {
+        const cardHTML =  `
+        <div class="col-12 col-md-6 col-lg-3 mb-3">
+        <img class="image w-100 mt-5 mb-4" src="${item.image}"/>
+        <div class="name">Name: ${item.name}</div>
+        <div class="description">Description: ${item.description}</div>
+        <div class="price">Price: ${item.price}</div>
+        </div>
+        `;
+        rowContainer.insertAdjacentHTML("beforeend", cardHTML)
+    })
+    
+}
+getData("products")
